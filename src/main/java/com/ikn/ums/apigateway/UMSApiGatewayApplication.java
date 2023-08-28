dd831d595a8b8747334d72c2a6410acf3390df90
Mahesh Jarati <mahesh.jarati@ikcontech.com>
2023-08-28 19:55:45 +0530

    initial commit

---
+++ src/main/java/com/ikn/ums/apigateway/UMSApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.ikn.ums.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableHystrix
+public class UMSApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UMSApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ikn/ums/apigateway/UMSApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikn.ums.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UMSApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
